9fe7d4f251f1a06434bf49ff566c16d61a033052
SRK.Lyu <superalsrk@gmail.com>
2017-02-19 18:34:58 +0800

    moidfy supervisor

--- application_event_listener_pro.py
+++ application_event_listener_pro.py
@@ -3,12 +3,20 @@
 
 import os
 import sys
+import json
+
+from supervisor import childutils
 
 
 def main():
-    while True:
-        pass
+    while 1:
+        headers, payload = childutils.listener.wait(sys.stdin, sys.stdout)
+        
+        with open('event.pro.log', 'a') as f:
+            f.write(json.dumps(headers))
 
+        childutils.listener.ok(sys.stdout)
+      
 
 if __name__ == '__main__':
     main()
